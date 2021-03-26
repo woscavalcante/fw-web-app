3e9ae4082e0c91342c65041650a43bd6afb18796
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-03-25 22:43:06 -0300

    Finalizado aula 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import "antd/dist/antd.css";
+import ListaPessoas from './components/ListaPessoas';
 
-function App() {
+const {Header, Content, Footer} = Layout;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout className="Layout">
+        <Header>
+          <div className="logo">
+            <Menu theme="dark" mode="horizontal" 
+            defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Home</Menu.Item>
+            </Menu>
+          </div>
+        </Header>
+        <Content style={{padding: '0 50px'}}>
+          <div className="site-layout-content">
+            <ListaPessoas></ListaPessoas>
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>eVaccine - FullStack Week</Footer>
+      </Layout>
     </div>
   );
 }
-
-export default App;
